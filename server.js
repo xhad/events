6aece1f98c14fbf2ab4a20d1f257900ad23f4ea7
Chad Lynch <chyad@hotmail.com>
2021-05-20 20:44:11 +0800

    feat: handle all pools

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-
-require('dotenv').config()
-const axios = require('axios')
-const { PoolSchema } = require('./schemas')
-const { EventEmitter } = require('events')
-const { transform, isArray, isEqual, isObject } = require('lodash')
+require("dotenv").config()
+const axios = require("axios")
+const { PoolSchema } = require("./schemas")
+const { EventEmitter } = require("events")
+const { transform, isArray, isEqual, isObject } = require("lodash")
 
 const event = new EventEmitter()
 
@@ -19,69 +18,86 @@ const allPools = `
 `
 
 async function postMessageToDiscord(content) {
-
-  const payload = JSON.stringify({content});
+  const payload = JSON.stringify({ content })
   const params = {
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
     url: process.env.WEBHOOK_URL,
     method: "POST",
     data: payload,
-    muteHttpExceptions: true
-  };
+    muteHttpExceptions: true,
+  }
   console.log(content)
 
   axios(params)
 }
 
 function difference(origObj, newObj) {
-  const changes= (newObj, origObj) => {
+  const changes = (newObj, origObj) => {
     let arrayIndexCounter = 0
     return transform(newObj, function (result, value, key) {
       if (!isEqual(value, origObj[key])) {
         let resultKey = isArray(origObj) ? arrayIndexCounter++ : key
-        result[resultKey] = (isObject(value) && isObject(origObj[key])) ? changes(value, origObj[key]) : value
+        result[resultKey] =
+          isObject(value) && isObject(origObj[key])
+            ? changes(value, origObj[key])
+            : value
       }
     })
   }
   return changes(newObj, origObj)
 }
 
-async function getPools (poolPositions) {
-    const PRIVATE_API = 'https://api.maple.finance/public'
-    const launchPool = await axios.post(PRIVATE_API, { query: allPools })
-  
-    const pool = launchPool.data.data.allPools.list[0]
-    if (!poolPositions.length) return event.emit('update', pool.poolPositions)
-    const lpAddress = pool.poolPositions[0].id.split('-')[0]
+async function getPools(poolPositions) {
+  const API_URL = process.env.API_URL
+  const request = await axios.post(API_URL, { query: allPools })
 
-    pool.poolPositions.forEach((position, i) => {
-      const a = JSON.stringify(poolPositions[i])
-      const b = JSON.stringify(position)
-      if (a !== b) {
-        const diff = difference(poolPositions[i], position)
-        event.emit('update', pool.poolPositions)
-        const message = "**Launch Pool Position Update** for `" + lpAddress + "`" + "\n" + "```" + JSON.stringify(poolPositions[i]) + "\n\n Updated Value(s): \n" + JSON.stringify(diff) + "```"
-        postMessageToDiscord(message)
-      }
+  const pools = request.data.data.allPools.list
+
+  const positions = await pools.map((pool, i) =>
+    Object.assign({
+      poolAddress: pool.contractAddress,
+      positions: pool.poolPositions,
     })
-  
+  )
 
+  positions.forEach((pool, i) => diffPositions(pool, poolPositions[i]))
+  event.emit("update", positions)
 }
 
-async function main () {
+async function diffPositions(pool, oldPool) {
+  pool.positions.forEach((position, i) => {
+    const a = JSON.stringify(oldPool.positions[i])
+    const b = JSON.stringify(position)
+
+    if (a !== b) {
+      const diff = difference(oldPool.positions[i], position)
+      const message =
+        "**Position Update** on pool " +
+        pool.poolAddress +
+        "\n" +
+        "```" +
+        JSON.stringify(oldPool.positions[i]) +
+        "\n\n Updated Value(s): \n" +
+        JSON.stringify(diff) +
+        "```"
+      postMessageToDiscord(message)
+    }
+  })
+}
+
+async function main() {
   try {
     let poolPositions = []
     console.log(`[${Date.now()}] Watching Launch pool`)
 
     setInterval(async () => getPools(poolPositions), 5000)
 
-    event.on('update', newPositions => {
-      poolPositions = newPositions 
+    event.on("update", (newPositions) => {
+      poolPositions = newPositions
     })
-
-  } catch(err) {
+  } catch (err) {
     console.log(err)
   }
 }
